24281d5232c1a2eee9dbe6930d8c0a5d887fb788
BuzzingEveY <111785575+BuzzingEveY@users.noreply.github.com>
2022-09-19 21:00:32 -0400

    Update train_classifier.py

--- disaster_response_pipeline_project/models/train_classifier.py
+++ disaster_response_pipeline_project/models/train_classifier.py
@@ -1,52 +1,100 @@
 import sys
+import pandas as pd
+import numpy as np
+import sqlite3
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    """
+      Function:
+      load data from two csv file and then merge them
+      Args:
+      messages_filepath (str): the file path of messages csv file
+      categories_filepath (str): the file path of categories csv file
+      Return:
+      df (DataFrame): A dataframe of messages and categories
+      """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, how='inner', on='id')
+    return df
 
 
-def load_data(database_filepath):
-    pass
+def clean_data(df):
+    """
+      Function:
+      clean the Dataframe df
+      Args:
+      df (DataFrame): A dataframe of messages and categories need to be cleaned
+      Return:
+      df (DataFrame): A cleaned dataframe of messages and categories
+      """
+
+    # Split `categories` into separate category columns.
+    categories = df['categories'].str.split(';', expand=True)
+
+    # Cut the last character of each category
+    # select the first row of the categories dataframe
+    row = categories.head(1)
+    category_colnames = row.applymap(lambda x: x[:-2]).iloc[0, :]
+    category_colnames = category_colnames.tolist()
+
+    # Rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # Convert category values to just numbers 0 or 1.
+    for column in categories:
+        categories[column] = categories[column].astype(str).str[-1]
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df = df.drop(['categories'], axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1, join='inner')
+
+    # Drop the duplicates.
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
-def tokenize(text):
-    pass
-
-
-def build_model():
-    pass
-
-
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
-
-def save_model(model, model_filepath):
-    pass
+def save_data(df, database_filename):
+    """
+       Function:
+       Save the Dataframe df in a database
+       Args:
+       df (DataFrame): A dataframe of messages and categories
+       database_filename (str): The file name of the database
+       """
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('disaster_messages_tbl', engine, index=False)
 
 
 def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
-        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
-        print('Building model...')
-        model = build_model()
-        
-        print('Training model...')
-        model.fit(X_train, Y_train)
-        
-        print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+    if len(sys.argv) == 4:
 
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-        print('Trained model saved!')
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
 
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
 
 
 if __name__ == '__main__':
